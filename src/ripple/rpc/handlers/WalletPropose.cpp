c39b1dcc3ccc64856e9358420961eaf2c3dcefc5
CryptoManiac <balthazar@yandex.ru>
2019-11-29 17:33:03 +0300

    Add WIF and raw private key to WalletPropose; Replace ripple alphabet with bitcoin one.

--- src/ripple/rpc/handlers/WalletPropose.cpp
+++ src/ripple/rpc/handlers/WalletPropose.cpp
@@ -111,7 +111,10 @@ Json::Value walletPropose (Json::Value const& params)
     if (!keyType)
         keyType = KeyType::secp256k1;
 
-    auto const publicKey = generateKeyPair (*keyType, *seed).first;
+
+    auto const keyPair = generateKeyPair (*keyType, *seed);
+    auto const publicKey = keyPair.first;
+    auto const secretKey = keyPair.second;
 
     Json::Value obj (Json::objectValue);
 
@@ -126,6 +129,8 @@ Json::Value walletPropose (Json::Value const& params)
     obj[jss::public_key] = toBase58(TokenType::AccountPublic, publicKey);
     obj[jss::key_type] = to_string (*keyType);
     obj[jss::public_key_hex] = strHex (publicKey);
+    obj[jss::secret_key_hex] = strHex (secretKey);
+    obj[jss::secret_key_wif] = toWIF (secretKey);
 
     // If a passphrase was specified, and it was hashed and used as a seed
     // run a quick entropy check and add an appropriate warning, because
